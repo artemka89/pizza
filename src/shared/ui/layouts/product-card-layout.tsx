3345ce3b72445af3991500f1caa2cc807bfdd48b
Moshniaga Artem <artem.m89@mail.ru>
2024-07-19 22:52:09 +0300

    fix pizza card

--- src/shared/ui/layouts/product-card-layout.tsx
+++ src/shared/ui/layouts/product-card-layout.tsx
@@ -1,34 +1,41 @@
 import { FC } from 'react';
 
-interface ProductCardLayoutProps {
+type Item = {
+  id: string;
   name: string;
   description: string;
   startPrice: number;
+};
+
+interface ProductCardLayoutProps {
+  item: Item;
   action?: React.ReactNode;
   imageUrl: () => URL;
 }
 
 export const ProductCardLayout: FC<ProductCardLayoutProps> = ({
-  name,
-  description,
-  startPrice,
+  item,
   action,
   imageUrl,
 }) => {
   return (
     <div className='flex h-[418px] cursor-pointer flex-col items-center p-4'>
       <div className='h-[220px] w-[220px] transition-transform hover:translate-y-1'>
-        <img src={imageUrl().toString()} alt={name} className='h-full w-full' />
+        <img
+          src={imageUrl().toString()}
+          alt={item.name}
+          className='h-full w-full'
+        />
       </div>
       <h4 className='pb-1 pt-2 text-center text-xl font-bold leading-tight'>
-        {name}
+        {item.name}
       </h4>
       <div className='flex-1 overflow-hidden text-center text-sm text-muted-foreground'>
-        <p className='line-clamp-4'>{description}</p>
+        <p className='line-clamp-4'>{item.description}</p>
       </div>
 
       <div className='flex w-full items-center justify-between'>
-        <span className='text-lg font-bold'>от {startPrice} ₽</span>
+        <span className='text-lg font-bold'>от {item.startPrice} ₽</span>
         {action}
       </div>
     </div>
