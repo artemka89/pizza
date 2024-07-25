5f061684ea78e12f26936ce8af76ff1632a6ee99
Moshniaga Artem <artem.m89@mail.ru>
2024-07-25 15:24:35 +0300

    add option switcher

---
+++ src/features/product-details/ui/option-switcher.tsx
@@ -0,0 +1,66 @@
+import { FC } from 'react';
+
+import { cn } from '@/shared/lib/cn';
+
+type Option = {
+  value: string;
+  name?: string;
+  disabled: boolean;
+};
+
+interface OptionSwitcherProps {
+  options: Option[];
+  quantityType?: string;
+  className?: string;
+  setOptionValue: (value: string) => void;
+  activeOptionValue: string;
+}
+
+export const OptionSwitcher: FC<OptionSwitcherProps> = ({
+  options,
+  quantityType,
+  className,
+  activeOptionValue,
+  setOptionValue,
+}) => {
+  const activeOption = options.findIndex(
+    (option) => option.value === activeOptionValue,
+  );
+
+  return (
+    <div
+      className={cn(
+        'relative my-2 flex h-8 w-full overflow-hidden rounded-full bg-muted',
+        className,
+      )}>
+      {options.length > 1 && (
+        <div
+          className={cn(
+            'absolute bottom-0 left-0 top-0 p-0.5 transition-transform',
+            'before:block before:h-full before:w-full before:rounded-full before:bg-background before:drop-shadow-xl',
+          )}
+          style={{
+            width: `${100 / options.length}%`,
+            transform: `translateX(${activeOption * 100}%)`,
+          }}></div>
+      )}
+
+      {options.map((option) => (
+        <button
+          key={option.value}
+          disabled={option.disabled}
+          onClick={() => setOptionValue(option.value)}
+          className={cn(
+            'z-10 flex flex-1 items-center justify-center py-1 text-xs font-medium text-foreground',
+            option.value === activeOptionValue
+              ? 'cursor-default text-primary'
+              : 'cursor-pointer hover:drop-shadow',
+            option.disabled &&
+              'disabled:pointer-events-none disabled:opacity-50',
+          )}>
+          {option.name} {quantityType}
+        </button>
+      ))}
+    </div>
+  );
+};
