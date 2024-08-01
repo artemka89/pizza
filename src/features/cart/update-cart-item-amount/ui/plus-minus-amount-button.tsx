49a609cba84829d47bcb04d60bf55c02f16eaacc
Moshniaga Artem <artem.m89@mail.ru>
2024-08-01 22:09:57 +0300

    fix update cart item button

--- src/features/cart/update-cart-item-amount/ui/plus-minus-amount-button.tsx
+++ src/features/cart/update-cart-item-amount/ui/plus-minus-amount-button.tsx
@@ -14,21 +14,23 @@ export const PlusMinusAmountButton: FC<PlusMinusAmountButtonProps> = ({
   cartItemId,
   amount,
 }) => {
-  const UpdateCartItemAmount = useUpdateCartItemAmount();
+  const updateCartItemAmount = useUpdateCartItemAmount();
 
   const onCLickPlus = () => {
-    UpdateCartItemAmount.mutate({ id: cartItemId, amount: amount + 1 });
+    if (amount >= 99) return;
+    updateCartItemAmount.mutate({ id: cartItemId, amount: amount + 1 });
   };
 
   const onClickMinus = () => {
-    UpdateCartItemAmount.mutate({ id: cartItemId, amount: amount - 1 });
+    if (amount <= 1) return;
+    updateCartItemAmount.mutate({ id: cartItemId, amount: amount - 1 });
   };
 
   return (
     <div className='flex items-center gap-2'>
       <Button
         onClick={onClickMinus}
-        disabled={UpdateCartItemAmount.isPending}
+        disabled={amount <= 1 || updateCartItemAmount.isPending}
         variant='outline'
         size='icon'
         className='size-7 rounded-lg p-0'>
@@ -37,7 +39,7 @@ export const PlusMinusAmountButton: FC<PlusMinusAmountButtonProps> = ({
       <span className='font-bold'>{amount}</span>
       <Button
         onClick={onCLickPlus}
-        disabled={UpdateCartItemAmount.isPending}
+        disabled={amount >= 99 && updateCartItemAmount.isPending}
         variant='outline'
         size='icon'
         className='size-7 rounded-lg p-0'>
