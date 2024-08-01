a21907787a6a2329466161a4d6ab8bf1d72efd30
Moshniaga Artem <artem.m89@mail.ru>
2024-08-01 22:43:15 +0300

    fix dependencies, refactoring

--- src/features/product-details/compose/pizza-detail.tsx
+++ src/features/product-details/compose/pizza-detail.tsx
@@ -2,18 +2,17 @@ import { FC } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import { useSelectedItems } from '@/entities/products';
-// eslint-disable-next-line boundaries/entry-point, boundaries/element-types
-import { AddToCartButton } from '@/features/cart/add-cart-item/ui/add-to-cart-button';
-import { IngredientItem } from '@/features/product-details/ui/product-ingredient/ingredient-item';
 import { ProductModalLayout } from '@/shared/ui/layouts/product-modal-layout';
 import { SwitchButtons } from '@/shared/ui/switch-buttons';
 
+import { AddToCartButton } from '../add-cart-item/ui/add-to-cart-button';
 import { ACTIVE_PIZZA_SIZE, PIZZA_SIZES } from '../lib/constants';
 import { Pizza } from '../model/types/pizza';
 import { useMappedOptionToParam } from '../model/use-mapped-option-to-param';
 import { OptionParamText } from '../ui/option-param-text';
 import { PizzaImage } from '../ui/pizza-detail/pizza-image';
 import { ProductDetailLayout } from '../ui/product-detail-layout';
+import { IngredientItem } from '../ui/product-ingredient/ingredient-item';
 import { ProductIngredientList } from '../ui/product-ingredient/product-ingredient-list';
 
 export const PizzaDetail: FC<{ data: Pizza }> = ({ data }) => {
