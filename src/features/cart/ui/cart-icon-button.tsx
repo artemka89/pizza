2a6fa67ce6b2051545a6f98c01e754561851c1c1
Moshniaga Artem <artem.m89@mail.ru>
2024-08-05 14:10:42 +0300

    refactor: total amount and total price in cart icon button

--- src/features/cart/ui/cart-icon-button.tsx
+++ src/features/cart/ui/cart-icon-button.tsx
@@ -6,6 +6,9 @@ import { useGetUser } from '@/entities/user';
 import { cn } from '@/shared/lib/cn';
 import { Button } from '@/shared/ui/button';
 
+import { getTotalAmount } from '../lib/get-total-amount';
+import { getTotalPrice } from '../lib/get-total-price';
+
 interface CartIconButtonProps {
   className?: string;
 }
@@ -15,27 +18,22 @@ export const CartIconButton: FC<CartIconButtonProps> = ({ className }) => {
   const user = useGetUser();
   const { data } = useGetCart(user.data?.id || '');
 
-  const totalAmount = data?.cartItem.reduce(
-    (acc, item) => acc + item.amount,
-    0,
-  );
-  const totalPrice = data?.cartItem.reduce(
-    (acc, item) => acc + item.amount * item.option.price,
-    0,
-  );
+  const totalAmount = getTotalAmount(data?.cartItem);
+
+  const totalPrice = getTotalPrice(data?.cartItem);
 
   const onClickButton = () => {
-    if (!user) return;
+    if (!data) return;
     setShowCart(true);
   };
 
   return (
     <Button onClick={onClickButton} className={cn('group relative', className)}>
-      <span>{totalPrice || 0} ₽</span>
+      <span>{totalPrice} ₽</span>
       <span className='mx-3 h-full w-[1px] bg-primary-foreground/40' />
       <div className='flex items-center gap-1 transition duration-300 group-hover:opacity-0'>
         <ShoppingCart size={16} />
-        <span>{totalAmount || 0}</span>
+        <span>{totalAmount}</span>
       </div>
       <ArrowRight className='absolute right-5 w-5 -translate-x-2 opacity-0 transition duration-300 group-hover:translate-x-0 group-hover:opacity-100' />
     </Button>
