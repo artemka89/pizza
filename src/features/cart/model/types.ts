9d9cec13375a20c5d7b33a804f4112e0311ed202
Moshniaga Artem <artem.m89@mail.ru>
2024-08-22 23:21:56 +0300

    fix: option field interface in cart

--- src/features/cart/model/types.ts
+++ src/features/cart/model/types.ts
@@ -2,7 +2,7 @@ export interface CartItemType {
   id: string;
   product: { id: string; name: string; imageUrl: string };
   category: { name: string };
-  option: { size: number; price: number; weight?: number };
+  option: { size?: number; price: number; weight?: number; volume?: number };
   amount: number;
   ingredients: { name: string; price: number }[];
 }
