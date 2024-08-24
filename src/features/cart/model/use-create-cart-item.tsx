366f3f554675ac76e61c879c9199fcfbe57b6b97
Moshniaga Artem <artem.m89@mail.ru>
2024-08-24 14:48:53 +0300

    refactor: move create cart item logic in hook use-crate-cart-item

--- src/features/cart/model/use-create-cart-item.tsx
+++ src/features/cart/model/use-create-cart-item.tsx
@@ -1,23 +1,88 @@
 import { useMutation } from '@tanstack/react-query';
 
-import { useInvalidateCart } from '@/entities/cart';
+import { useGetCart, useInvalidateCart } from '@/entities/cart';
+import { useSelectedItems } from '@/entities/product';
+import { useGetUser } from '@/entities/user';
 import { cartApi } from '@/shared/api/models/cart';
+import { useToast } from '@/shared/ui/use-toast';
+
+interface CreateCartItem {
+  cartId: string;
+  cartItemIds: string[];
+  productId: string;
+  optionId: string;
+  categoryId: string;
+  ingredientsIds: string[];
+  amount: number;
+}
 
 export function useCreateCartItem() {
+  const { toast } = useToast();
   const invalidateCart = useInvalidateCart();
-  return useMutation({
+
+  const user = useGetUser();
+  const cart = useGetCart(user.data?.id || '');
+
+  const [selectedOption, selectedIngredients] = useSelectedItems((state) => [
+    state.option,
+    state.ingredients,
+  ]);
+
+  const cartItemIds = cart.data?.cartItem.map((item) => item.id) || [];
+  const ingredientsIds = selectedIngredients.map((item) => item.id) || [];
+
+  const selectedOptionId = selectedOption?.id;
+  const enabled = !!user.data && !!cart.data && !!selectedOptionId;
+
+  const mutate = useMutation({
     mutationKey: ['add-cart'],
-    mutationFn: (data: {
-      cartId: string;
-      cartItemIds: string[];
-      productId: string;
-      optionId: string;
-      categoryId: string;
-      ingredientsIds: string[];
-      amount: number;
-    }) => cartApi.addItem(data),
+    mutationFn: (data: CreateCartItem) => cartApi.addItem(data),
     onSettled: async () => {
       await invalidateCart();
     },
+    onSuccess: () => {
+      toast({
+        variant: 'success',
+        title: 'Товар добавлен в корзину',
+      });
+    },
+    onError: () => {
+      toast({
+        variant: 'destructive',
+        title: 'Ошибка добавления в корзину',
+        description: 'Повторите попытку',
+      });
+    },
   });
+
+  const create = ({
+    categoryId,
+    productId,
+  }: {
+    categoryId: string;
+    productId: string;
+  }) => {
+    if (!enabled) {
+      toast({
+        variant: 'destructive',
+        title: 'Ошибка добавления в корзину',
+        description: 'Повторите попытку',
+      });
+      return;
+    }
+
+    const data: CreateCartItem = {
+      cartId: user.data.id,
+      cartItemIds: cartItemIds,
+      productId,
+      optionId: selectedOptionId,
+      categoryId,
+      ingredientsIds: ingredientsIds,
+      amount: 1,
+    };
+
+    mutate.mutate(data);
+  };
+
+  return { create, enabled, isPending: mutate.isPending };
 }
--- src/features/cart/ui/add-to-cart-button.tsx
+++ src/features/cart/ui/add-to-cart-button.tsx
@@ -2,10 +2,7 @@ import { FC } from 'react';
 
 import { useSelectedItems } from '@/entities/product';
 import { Button } from '@/shared/ui/button';
-import { useToast } from '@/shared/ui/use-toast';
 
-import { useGetCartItemToAdd } from '../lib/use-get-cart-item-to-add';
-import { useGetCartItemToUpdate } from '../lib/use-get-cart-item-to-update';
 import { useCreateCartItem } from '../model/use-create-cart-item';
 import { useUpdateCartItemAmount } from '../model/use-update-cart-item-amount';
 
@@ -17,36 +14,14 @@ export const AddToCartButton: FC<AddToCartButtonProps> = ({
   productId,
   categoryId,
 }) => {
-  const { toast } = useToast();
+  const [price] = useSelectedItems((state) => [state.price]);
 
   const createCartItem = useCreateCartItem();
   const updateCartItem = useUpdateCartItemAmount();
 
-  const [price] = useSelectedItems((state) => [state.price]);
-
-  const cartItemToAdd = useGetCartItemToAdd(productId, categoryId);
-  const cartItemToUpdate = useGetCartItemToUpdate(productId);
-
   const addToCart = () => {
-    if (cartItemToUpdate) {
-      updateCartItem.mutate(cartItemToUpdate, {
-        onSuccess: () => {
-          toast({
-            variant: 'success',
-            title: 'Товар добавлен в корзину',
-          });
-        },
-      });
-    } else {
-      cartItemToAdd &&
-        createCartItem.mutate(cartItemToAdd, {
-          onSuccess: () => {
-            toast({
-              variant: 'success',
-              title: 'Товар добавлен в корзину',
-            });
-          },
-        });
+    if (createCartItem.enabled) {
+      createCartItem.create({ categoryId, productId });
     }
   };
 
